cc71fbcbcfde355de1c9a615df8b92d7f066151e
Arteen Abrishami <arteen1000@gmail.com>
2023-09-16 12:47:38 -0700

    update for new emacs

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  '(Man-switches "-a")
  '(custom-enabled-themes '(tango-dark))
  '(inhibit-startup-screen t)
- '(package-selected-packages '(solarized-theme dired-subtree rainbow-delimiters)))
+ )
 
 ;; convenience features
 
@@ -48,6 +48,11 @@
 (global-set-key (kbd "H-s o") 'eshell-other-window)
 (global-set-key (kbd "H-s H-s") 'eshell)
 
+;; bind `man's
+(global-set-key (kbd "H-m o") 'man-other-window)
+(global-set-key (kbd "H-m H-m") 'man)
+
+
 ;; fix alias warning since ls doesn't support --dired
 
 (when (string= system-type "darwin")       
@@ -55,24 +60,24 @@
 
 ;; fix man-auto complete slowness
 
-(declare-function 'Man-default-man-entry "man")
+;; (declare-function 'Man-default-man-entry "man")
 
-(define-advice man (:around (orig-func &rest args) no-completing-read)
-  "Inhibit `completing-read'."
-  (interactive
-   (list (let* ((default-entry (Man-default-man-entry))
-                (input (read-string
-                        (format-prompt "Manual entry"
-                                       (and (not (equal default-entry ""))
-                                            default-entry))
-                        nil 'Man-topic-history default-entry)))
-           (if (string= input "")
-               (error "No man args given")
-             input))))
-  (apply orig-func args))
+;; (define-advice man (:around (orig-func &rest args) no-completing-read)
+;;   "Inhibit `completing-read'."
+;;   (interactive
+;;    (list (let* ((default-entry (Man-default-man-entry))
+;;                 (input (read-string
+;;                         (format-prompt "Manual entry"
+;;                                        (and (not (equal default-entry ""))
+;;                                             default-entry))
+;;                         nil 'Man-topic-history default-entry)))
+;;            (if (string= input "")
+;;                (error "No man args given")
+;;              input))))
+;;   (apply orig-func args))
 
-;; Undo with:
-(advice-remove 'man 'man@no-completing-read)
+;; ;; Undo with:
+;; (advice-remove 'man 'man@no-completing-read)
 
 (defun connect-hexaconta ()
   "let me `ssh' into hexaconta"
@@ -96,7 +101,6 @@
 ;; tramp is annoying
 (setq tramp-auto-save-directory "/tmp")
 
-
 ;; don't pop windows up without my permission
 ;; (setq pop-up-windows nil)
 
@@ -114,44 +118,44 @@
 
 ;; god help me
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; (require 'package)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 ;; because i'm too lazy to install tree-sitter
-(use-package rainbow-delimiters
-  :ensure t)
+;; (use-package rainbow-delimiters
+;;   :ensure t)
 
 ;; for rainbow delims when programming
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+;; (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
 ;; try a fix for grep ...
 
-(defun my-compile-goto-error-same-window ()
-  (interactive)
-  (let ((display-buffer-overriding-action
-         '((display-buffer-reuse-window
-            display-buffer-same-window)
-           (inhibit-same-window . nil))))
-    (call-interactively #'compile-goto-error)))
+;; (defun my-compile-goto-error-same-window ()
+;;   (interactive)
+;;   (let ((display-buffer-overriding-action
+;;          '((display-buffer-reuse-window
+;;             display-buffer-same-window)
+;;            (inhibit-same-window . nil))))
+;;     (call-interactively #'compile-goto-error)))
 
-(defun my-compilation-mode-hook ()
-  (local-set-key (kbd "<return>") #'my-compile-goto-error-same-window))
+;; (defun my-compilation-mode-hook ()
+;;   (local-set-key (kbd "<return>") #'my-compile-goto-error-same-window))
 
-(add-hook 'compilation-mode-hook #'my-compilation-mode-hook)
+;; (add-hook 'compilation-mode-hook #'my-compilation-mode-hook)
 
 ;; i really don't want you to mess with my windows
 
-(customize-set-variable 'display-buffer-base-action
-                        '((display-buffer-reuse-window display-buffer-same-window)
-                          (reusable-frames . t)))
+;; (customize-set-variable 'display-buffer-base-action
+;;                         '((display-buffer-reuse-window display-buffer-same-window)
+;;                           (reusable-frames . t)))
 
 ;; hello
-(use-package dired-subtree
-        :ensure t
-        :bind (:map dired-mode-map
-                    ("i" . dired-subtree-insert)
-                    (";" . dired-subtree-remove)
-                    ("<tab>" . dired-subtree-toggle)
-                    ("<backtab>" . dired-subtree-cycle)))
+;; (use-package dired-subtree
+;;         :ensure t
+;;         :bind (:map dired-mode-map
+;;                     ("i" . dired-subtree-insert)
+;;                     (";" . dired-subtree-remove)
+;;                     ("<tab>" . dired-subtree-toggle)
+;;                     ("<backtab>" . dired-subtree-cycle)))
 
-(setq native-comp-async-report-warnings-errors 'silent)
+;; (setq native-comp-async-report-warnings-errors 'silent)
