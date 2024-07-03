16aa1f62f81c567219a3bacfbbe934bd6fb6563a
Arteen Abrishami <arteen1000@gmail.com>
2024-07-02 21:36:15 -0700

    update

--- early-init.el
+++ early-init.el
@@ -1,16 +1,3 @@
-;; --:**- early-init.el --
-;; Code:
-
-;; these affect the GUI and the way it looks
-;; and cause an unpleasant hang (and a white flash,
-;; if done before the theme is initialized) which
-;; is unpleasant for the soul
-
-;; we do them before the GUI is initialized
-
-(tool-bar-mode -1) ;; get rid of top level toolbar
-(scroll-bar-mode -1) ;; get rid of scroll bars
-
-;; get rid of the menu bar, but only in the terminal (non-intrusive in GUI, in top-left macOS screen)
-(if (not (display-graphic-p))
-    (menu-bar-mode -1))
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
--- init.el
+++ init.el
@@ -1,73 +1,22 @@
-;;; --:**- init.el --  -*- lexical-binding: t; -*-
-;; Code:
-
-;; Note: git-timemachine may be useful at some point
-
-(setq ns-function-modifier 'hyper) ;; nextstep fn (macOS) to hyper
-(setq view-read-only t) ;; view mode on when read-only
-(setq column-number-mode t) ;; want to see column number
-(setq-default indent-tabs-mode nil) ;; indentation can't insert tabs
-(setq-default tab-width 4) ;; make tab-width 4 (spaces)
+;; useful: delete-blank-lines
+;; begin
+(setq ns-function-modifier 'hyper)
+(global-hl-line-mode)
+(when (string= system-type "darwin")
+  (setq dired-use-ls-dired nil))
+(setq inhibit-startup-screen t)
+(setq ring-bell-function 'ignore)
+(setq column-number-mode t)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 2)
+;; (setq indent-line-function 'insert-tab)
 (setq tramp-auto-save-directory "/tmp")
-(global-auto-revert-mode) ;; refresh buffer if file changed on disk
-;; (fset 'yes-or-no-p 'y-or-n-p) ;; set yes/no prompt to y/n
-(setq use-short-answers t) ;; cleaner way to do the above, affects more funcs too
-;; (visual-line-mode) ;; make it work on "visual" lines instead of logical
-(delete-selection-mode) ;; if I have a region active and I type, get rid of the region
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(setq scroll-error-top-bottom t) ;; if my movement makes me go past end of buf (or beg), just take me there, instead of signalling error straight away
-
-;; restore window configuration, window history
-;; allow to undo frame window-split changes with C-c <left> and re-do with C-c <right>
-(winner-mode) 
-
-;; makes consistent with shell
-;; but this messes with mark being set before things like M->
-;; (defadvice kill-region (before unix-werase activate compile)
-;;   "When called interactively with no active region, delete a single word backwards instead."
-;;   (interactive
-;;    (if mark-active (list (region-beginning) (region-end))
-;;      (list (save-excursion (backward-word 1) (point)) (point)))))
-
-(setq native-comp-async-report-warnings-errors 'silent)
-(add-hook 'prog-mode-hook #'hs-minor-mode) ;; code folding
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(Man-notify-method 'pushy)
- '(Man-switches "-a")
- '(inhibit-startup-screen t)
- '(package-native-compile t)
- '(package-selected-packages
-   '(dtrt-indent counsel-projectile counsel projectile company lsp-java lsp-mode magit vterm rust-mode pdf-tools latex-preview-pane utop tuareg dired-sidebar dired-subtree expand-region doom-themes))
- '(send-mail-function 'mailclient-send-it)
- '(treesit-font-lock-level 4))
-
-(defun man-other-window ()
-  "open a `man' in a new window."
-  (interactive)
-  (let ((buf (call-interactively 'man)))
-	(switch-to-buffer (other-buffer buf))
-	(switch-to-buffer-other-window buf)))
-
-(defun eshell-other-window ()
- "Open an `eshell' in a new window."
- (interactive)
- (let ((buf (eshell)))
-   (switch-to-buffer (other-buffer buf))
-    (switch-to-buffer-other-window buf)))
-
-(global-set-key (kbd "H-v o") 'vterm-other-window)
-(global-set-key (kbd "H-v H-v") 'vterm)
-
-(global-set-key (kbd "H-m o") 'man-other-window)
-(global-set-key (kbd "H-m H-m") 'man)
-
-(global-set-key (kbd "H-o") 'occur) ;; grep-like find lines action within same file
-
-(defun toggle-window-split ()
+(global-auto-revert-mode)
+(setq use-short-answers t)
+(delete-selection-mode)
+(setq scroll-error-top-bottom t)
+(winner-mode) ;; C-c <left> and C-c <right>
+(defun transpose-windows ()
   (interactive)
   (if (= (count-windows) 2)
       (let* ((this-win-buffer (window-buffer))
@@ -91,81 +40,32 @@
           (set-window-buffer (next-window) next-win-buffer)
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
-
-;; (add-to-list 'load-path "~/.emacs.d/orphan-packages/")
-;; (require 'transpose-frame)
-(global-set-key (kbd "H-t") 'toggle-window-split)
-
-(defun comment-or-uncomment-region-or-line ()
-    "Comments or uncomments the region or the current line if there's no active region."
-    (interactive)
-    (let (beg end)
-        (if (region-active-p)
-            (setq beg (region-beginning) end (region-end))
-            (setq beg (line-beginning-position) end (line-end-position)))
-        (comment-or-uncomment-region beg end)))
-
-(global-set-key (kbd "H-w") 'comment-or-uncomment-region-or-line)
-
-;; (global-set-key (kbd "H-w") 'window-swap-states)
-
-;; want an easy way to move between windows
-;; firstly, remove C-<arrow keys> keybindings for macOS
-;; Settings -> Keyboard Shortcuts -> Mission Control
-;; rebind to use <CMD> key instead, then apply this
-;; to let us navigate windows with C-<arrow keys>
-;; notably: the UP and DOWN keys are bound to "Mission Control"
-;; and "Applications Windows"
 (windmove-default-keybindings 'control)
-
-;; now, these allow us to swap our window states (buffers)
 (windmove-swap-states-default-keybindings '(shift control))
-
-;; and finally, (this overrides previous M-<up> and M-<down>
-;; but it's fine, because equivalent functionality for left-to-right
-;; languages is present in M-f and M-b
-(windmove-delete-default-keybindings 'none 'meta)
-
-
-;; this will delete surrounding newlines
-(global-set-key (kbd "C-S-<backspace>") 'delete-blank-lines)
-
-
-(when (string= system-type "darwin")       
-  (setq dired-use-ls-dired nil)) ;; darwin ls doesn't support --dired
-
-;; (fix?) man auto-complete slowness on MacOS
-
-(declare-function 'Man-default-man-entry "man")
-
-(define-advice man (:around (orig-func &rest args) no-completing-read)
-  "Inhibit `completing-read'."
-  (interactive
-   (list (let* ((default-entry (Man-default-man-entry))
-                (input (read-string
-                        (format-prompt "Manual entry"
-                                       (and (not (equal default-entry ""))
-                                            default-entry))
-                        nil 'Man-topic-history default-entry)))
-           (if (string= input "")
-               (error "No man args given")
-             input))))
-  (apply orig-func args))
-
-;; Undo with:
-;; (advice-remove 'man 'man@no-completing-read)
-
-(defun connect-hexaconta ()
-  "let me `ssh' into hexaconta"
+;; begin hack
+(global-set-key (kbd "M-<left>") 'windmove-delete-left)
+(global-set-key (kbd "M-<right>") 'windmove-delete-right)
+(global-set-key (kbd "M-<up>") 'windmove-delete-up)
+(global-set-key (kbd "M-<down>") 'windmove-delete-down)
+;; end hack
+(defun connect ()
   (interactive)
-  (dired "/ssh:hexaconta:/home/arteen"))
+  (dired "/ssh:arm:/home/artabr01/"))
+(setq dired-omit-files
+      (rx (or (seq bol (? ".") "#")
+              (seq bol "." (not (any ".")))
+              (seq "~" eol))))
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map "b" 'dired-create-empty-file)
+     (define-key dired-mode-map "e" 'dired-omit-mode)
+     (define-key dired-mode-map (kbd "<tab>") 'dired-subtree-toggle)))
+;; begin
+(add-to-list 'same-window-buffer-names "*Buffer List*")
+(add-to-list 'same-window-buffer-names "*Help*")
+;; end
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
-(defun connect-lug ()
-  "let me `ssh' into lug"
-  (interactive)
-  (dired "/ssh:root@lug:/var/www/html"))
-
-;; initialize package sources
 (require 'package)
 (setq package-archives
       '(("gnu" . "https://elpa.gnu.org/packages/")
@@ -176,185 +76,44 @@
       '(("gnu" . 4)
         ("nongnu" . 3)
         ("melpa-stable" . 2)
-        ("melpa" . 1))
-      )
-
-;; make sure that environment variables correctly inherited from shell on MacOS
-;; even when not started from inside the shell
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (when (memq window-system '(mac ns x))
-   (exec-path-from-shell-initialize))
-  )
-
+        ("melpa" . 1)))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
 (use-package doom-themes
   :ensure t
   :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
   (load-theme 'doom-vibrant t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
-
-(setq dired-omit-files
-    (rx (or (seq bol (? ".") "#")     ;; emacs autosave files
-        (seq bol "." (not (any "."))) ;; dot-files
-        (seq "~" eol)                 ;; backup-files
-        )))
-
-(setq treesit-language-source-alist ;; set up treesit grammars
-      '(
-        (css "https://github.com/tree-sitter/tree-sitter-css")
-        (java "https://github.com/tree-sitter/tree-sitter-java")
-        (javascript "https://github.com/tree-sitter/tree-sitter-javascript")
-        (json "https://github.com/tree-sitter/tree-sitter-json")
-        (make "https://github.com/alemuller/tree-sitter-make")
-        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-        (python "https://github.com/tree-sitter/tree-sitter-python")
-        (c "https://github.com/tree-sitter/tree-sitter-c")
-        (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-        (rust "https://github.com/tree-sitter/tree-sitter-rust")
-        )
-      )
-
-;; (mapc #'treesit-install-language-grammar (mapcar #'car treesit-language-source-alist)) ;; run this to install grammars
-
-(setq major-mode-remap-alist ;; a bit hacky, for treesit modes
-      '((css-mode . css-ts-mode)
-        (javascript-mode . js-ts-mode)
-        (js-json-mode . json-ts-mode)
-        (c++-mode . c++-ts-mode)
-        (python-mode . python-ts-mode)
-        (java-mode . java-ts-mode)
-        )
-      )
-
-(use-package expand-region
-  :ensure t
-  :bind ("C-=" . er/expand-region))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
+  (doom-themes-visual-bell-config))
 (use-package dired-subtree
   :ensure t)
-
-(eval-after-load "dired"
-  '(progn
-	 (define-key dired-mode-map "b" 'dired-create-empty-file)
-     (define-key dired-mode-map "e" 'dired-omit-mode)
-     (define-key dired-mode-map (kbd "<tab>") 'dired-subtree-toggle)
-	 )
-  )
-
-(global-set-key (kbd "H-f") 'find-name-dired)
-(global-set-key (kbd "H-g") 'find-grep-dired)
-
-
-(add-to-list 'auto-mode-alist '("\\.sm\\'" . c++-mode))
-
-(setq c-basic-offset 2)
-(setq c-ts-mode-indent-offset 2)
-
-(setq java-ts-mode-indent-offset 2)
-
 (use-package dired-sidebar
   :ensure t
-  :bind (("H-k" . dired-sidebar-toggle-sidebar))
-  )
-
-;; Ocaml
-(use-package tuareg
-  :ensure t
-  :mode (("\\.ocamlinit\\'" . tuareg-mode)))
-
-(use-package utop
-  :ensure t)
-
-(setq utop-command "opam exec -- utop -emacs")
-
-(add-to-list 'same-window-buffer-names "*utop*")
-(add-to-list 'same-window-buffer-names "*Buffer List*")
-
-;; (use-package latex-preview-pane
-;;   :ensure t)
-
-;; (latex-preview-pane-enable)
-;; (add-hook 'LaTeX-mode-hook 'latex-preview-pane-mode)
-
-(use-package pdf-tools
-  :ensure t)
-
-(pdf-tools-install) 
-(pdf-loader-install)
-
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups/")))
-
-;; Prolog vs. Perl
-(add-to-list 'auto-mode-alist '("\\.\\(pl\\|pro\\|lgt\\)" . prolog-mode))
-
-;; rust support
-(use-package rust-mode
-  :ensure t
-  :init
-  (setq rust-mode-treesitter-derive t))
-
+  :bind (("H-k" . dired-sidebar-toggle-sidebar)))
 (use-package vterm
   :ensure t
   :config
   (setq vterm-timer-delay 0.01))
-
-;; try some newline QOL
-
-;; (bind-key "RET" #'reindent-then-newline-and-indent)
-
-;; (global-set-key
-;;  [remap newline]
-;;  `(menu-item "" default-indent-new-line :filter
-;;              ,(lambda (_cmd)
-;;                 (when (save-excursion (comment-beginning))
-;;                   `(lambda () (interactive) (,comment-line-break-function))))))
-
-
-;; (global-set-key (kbd "RET") (key-binding (kbd "M-j")))
-
-(use-package magit
-  :ensure t)
-
-(use-package clang-format
-  :ensure t)
-
-(defun clang-format-save-hook-for-this-buffer ()
-  "Create a buffer local save hook."
-  (add-hook 'before-save-hook
-            (lambda ()
-              (when (locate-dominating-file "." ".clang-format")
-                (clang-format-buffer))
-              ;; Continue to save.
-              nil)
-            nil
-            ;; Buffer local hook.
-            t))
-
-;; Run this for each mode you want to use the hook.
-;; (add-hook 'c-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
-(add-hook 'c++-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
-(add-hook 'c++-ts-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
-
-(use-package projectile
-  :ensure t)
-(use-package counsel
-  :ensure t)
+;; begin
+;; (use-package clang-format
+;;   :ensure t)
+;; (defun clang-format-save-hook-for-this-buffer ()
+;;   "Create a buffer local save hook."
+;;   (add-hook 'before-save-hook
+;;             (lambda ()
+;;               (when (locate-dominating-file "." ".clang-format")
+;;                 (clang-format-buffer))
+;;               ;; Continue to save.
+;;               nil)
+;;             nil
+;;             ;; Buffer local hook.
+;;             t))
+;; (add-hook 'c++-mode-hook (lambda () (clang-format-save-hook-for-this-buffer)))
+;; end
+;; begin
 (use-package counsel-projectile
   :ensure t)
 (counsel-mode)
@@ -376,7 +135,43 @@
 (add-to-list 'ivy-height-alist (cons 'counsel-find-file
                                      (lambda (_caller)
                                        (/ (frame-height) 3))))
+(setq counsel-find-file-ignore-regexp "\\`[#.]\\|[#~]\\'")
+(setq ivy-use-selectable-prompt t)
+;; begin
+(add-to-list 'load-path "~/.emacs.d/my-packages/llvm")
+(add-to-list 'load-path "~/.emacs.d/my-packages/clang-format-remote")
+(require 'tablegen-mode)
+;; end
+;; end
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+   (exec-path-from-shell-initialize)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(package-lint flycheck treesit-auto dired-subtree use-package doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+;; begin
+(setq treesit-font-lock-level 4)
+;; end
+;; begin
+(electric-indent-mode 0)
+(add-hook 'prog-mode-hook #'electric-indent-local-mode)
+;; end
+;; begin
+(require 'clang-format-remote)
+(add-hook 'c++-mode-hook #'clang-format-save-hook)
+;; end
 
-;; (use-package dtrt-indent
-;;   :ensure t)
-;; (dtrt-indent-global-mode)
+(use-package flycheck
+  :ensure t)
